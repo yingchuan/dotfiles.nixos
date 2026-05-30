6ec435ad2e5ec07e4173080222e1d484d4bfe177
Richard Chen <yingchuan.chen.2007@gmail.com>
2026-05-31 02:35:41 +0800

    feat(ssh): add richard's public key to authorizedKeys

--- hosts/shared/system-module.nix
+++ hosts/shared/system-module.nix
@@ -37,6 +37,9 @@
     description = "Richard Chen";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     autoSubUidGidRange = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0gDvM/oioX/WQhBubJHmPXTssqGKanuFmbHtxKkz0aBpuUqSOg2NawIpVZyfrrcJeE/2BSTx6MCF02gEE8DMZucxPr9qk400qWPT0lMydF2Wdz16mHmOlHMB/EKNgAx6FX1RIxtuvFRfYM/zPbIJHoYWZeMEp9BLa+jrPTj7bjWUvHq5zIOiTxArFdrD5epBDPfcLR6DynaG6ysxyMWZK645mJPC89HGNhyVDyLDwO43GxRDnBmNxcH79Hc4HoWClVgpgQMfemPUE3gKiFSokHBDULD4gbXRjqCbe10muI2SfE8kyBGXFffyJT3G/kK4ewVCzUQ3EmUUe+uRf7G3K/tSQc/adWZrUVnrpDBg4Op2/l2dnsSbdpDQri4LnL1gagVBg9fZJN0bV4him7M4cMU1WY9iUypYSXKqR5AmDObIfYZFoH0SlqL2xoyKoNEtFPaGlDpkIHEqxaT2k9paTUbiqZjKDh9iN/s1TjbTTqTXiGzB74vDF4xcpQCLcR078= richardchen@ES-C0184-MacBook-Pro-14.home"
+    ];
   };
 
   # Basic programs
